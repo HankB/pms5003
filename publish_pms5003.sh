117b90ec0a157906a9c62476fbf9dc7436f30a44
Hank Barta <hbarta@gmail.com>
2025-08-02 16:33:42 -0500

    shellcheck cleanup

---
+++ publish_pms5003.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+
+set -o errexit
+set -o pipefail
+set -o nounset
+############### end of Boilerplate
+
+# Run the command to read the PMS3005 sensor (which produces JSON 
+# output) 
+# Use `jq` to add the timestamp and sensor name and pipe to
+# mosquitto_pub to publish
+
+/home/hbarta/bin/pms5003 "/dev/ttyAMA0" 4 | \
+    /bin/jq -c '. += {"t":'"$(date +%s)"',"sensor":"PMS5003"}'| \
+    mosquitto_pub -h mqtt -l -t "HA/$(/bin/hostname)/roamer/particulates"
